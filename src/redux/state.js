7b8c21b0e9fe4a2f3f26d1876acc8a5ff3538e80
Your Name <you@example.com>
2022-01-23 14:32:58 +0200

    Change news title

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+    console.log('State changed');
+}
 
 let state = {
     profilePage: {
@@ -48,7 +50,7 @@ window.state = state;
 //     rerenderEntireTree(state);
 // };
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 3,
         message: state.profilePage.newPostText,
@@ -60,11 +62,15 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
     state.profilePage.newPostText= newText;
     rerenderEntireTree(state);
 };
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+}
 
 
 
